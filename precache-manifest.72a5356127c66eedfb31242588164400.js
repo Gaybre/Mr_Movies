2a063215329ed681b9957187b13a4ab8398ccc10
JuanGF <jugaybre@gmail.com>
2020-06-27 00:32:24 -0500

    Updates

---
+++ precache-manifest.72a5356127c66eedfb31242588164400.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b98692d6de8cb61d46b26dbe4121b67",
+    "url": "/Mr_Movies/index.html"
+  },
+  {
+    "revision": "6a22b6dba2e009aa93f1",
+    "url": "/Mr_Movies/static/css/main.ecf9563e.chunk.css"
+  },
+  {
+    "revision": "e0d0acc74a49f3a20a9b",
+    "url": "/Mr_Movies/static/js/2.461f2151.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Mr_Movies/static/js/2.461f2151.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a22b6dba2e009aa93f1",
+    "url": "/Mr_Movies/static/js/main.913f19e5.chunk.js"
+  },
+  {
+    "revision": "2fd7b03dabd3a09c1045",
+    "url": "/Mr_Movies/static/js/runtime-main.da5f4732.js"
+  }
+]);
